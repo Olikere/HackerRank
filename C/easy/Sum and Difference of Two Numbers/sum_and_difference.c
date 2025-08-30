eb49178ef4cc9718e87adf09745b7e2eb76ad652
Olikere <139827079+Olikere@users.noreply.github.com>
2025-08-30 19:43:23 -0300

    Create sum_and_difference.c
    code solution

---
+++ C/easy/Sum and Difference of Two Numbers/sum_and_difference.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int val_1, val_2;
+    float val_3, val_4;
+    
+    scanf("%d %d", &val_1, &val_2);
+    scanf ("%f %f",&val_3, &val_4);
+    
+    int soma = val_1 + val_2;
+    int dif = val_1 - val_2;
+
+    float soma2 = val_3 + val_4;
+    float dif2 = val_3 - val_4;
+    
+    
+    
+    printf("%d %d\n",soma, dif);
+    printf("%.1f %.1f\n", soma2, dif2);
+    
+    
+    
+    return 0;
+}
